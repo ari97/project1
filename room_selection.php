5089a1972e56b10a71d9214f2b79bf820f5a2ff2
mcelevan12 <evan.mcelheny1@marist.edu>
2016-02-07 17:50:34 +0000

    Initialize Repository

---
+++ room_selection.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Room Selection Results</title>
+    </head>
+    <?php
+        $hallSelection = $_POST["residence"];
+        $gender = $_POST["gender"];
+        echo $hallSelection;
+        $kitchen = $_POST["kitchen"];
+        echo $kitchen;
+        echo $_POST["laundry"];
+        
+    ?>
+</html>
