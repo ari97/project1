8dbe1c56c952ceb24143367207c26dd80884a34c
mcelevan12 <evan.mcelheny1@marist.edu>
2016-02-08 17:22:34 +0000

    Update TODO

--- index.php
+++ index.php
@@ -1,43 +1,43 @@
 <HTML>
-    <head>
-      <title>Room Selection Form</title>
-    </head>
-    <body>
-      <form action="room_selection.php" method="POST">
-        First name: <input type="text" name="fname"><br>
-        Last name: <input type = "text" name = "lname"><br>
-        CWID: <input type = "text" name = "cwid"><br>
-        <input type = "radio" name="gender" value = "male"> Male<br>
-        <input type = "radio" name="gender" value = "female"> Female<br>
-        <input type = "radio" name="gender" value = "other"> Other<br>
+  <head>
+    <title>Room Selection Form</title>
+  </head>
+  <body>
+    <form action="room_selection.php" method="POST">
+      First name: <input type="text" name="fname"><br>
+      Last name: <input type = "text" name = "lname"><br>
+      CWID: <input type = "text" name = "cwid"><br>
+      <input type = "radio" name="gender" value = "male"> Male<br>
+      <input type = "radio" name="gender" value = "female"> Female<br>
+      <input type = "radio" name="gender" value = "other"> Other<br>
+      
+      <input type = "radio" name="class" value = "freshman"> Freshman<br>
+      <input type = "radio" name="class" value = "sophmore"> Sophmore<br>
+      <input type = "radio" name="class" value = "junior"> Junior<br>
+      <input type = "radio" name="class" value = "senior"> Senior<br>
+      <select name = "residence">
+        <option selected disabled hidden value=''>Select a Residence Hall</option>
+        <option value="leo">Leo Hall</option>
+        <option value="champagnat">Champagnat Hall</option>
+        <option value="marian">Marian Hall</option>
+        <option value="sheahan">Sheahan Hall</option>
+        <option value="midrise">Midrise Hall</option>
+        <option value="foy">Foy Townhouses</option>
+        <option value="gartland">Gartland Commons</option>
+        <option value="new">New Townhouses</option>
+        <option value="lower_west">Lower West Cedar St Townhouses</option>
+        <option value="upper_west">Upper West Cedar St Townhouses</option>
+        <option value="fulton">Fulton Street Townhouses</option>
+        <option value="talmadge">Talmadge Court</option>
+        <option value="new_fulton">New Fulton Townhouses</option>
+      </select>
+      <br>
         
-        <input type = "radio" name="class" value = "freshman"> Freshman<br>
-        <input type = "radio" name="class" value = "sophmore"> Sophmore<br>
-        <input type = "radio" name="class" value = "junior"> Junior<br>
-        <input type = "radio" name="class" value = "senior"> Senior<br>
-        <select name = "residence">
-          <option selected disabled hidden value=''>Select a Residence Hall</option>
-          <option value="leo">Leo Hall</option>
-          <option value="champagnat">Champagnat Hall</option>
-          <option value="marian">Marian Hall</option>
-          <option value="sheahan">Sheahan Hall</option>
-          <option value="midrise">Midrise Hall</option>
-          <option value="foy">Foy Townhouses</option>
-          <option value="gartland">Gartland Commons</option>
-          <option value="new">New Townhouses</option>
-          <option value="lower_west">Lower West Cedar St Townhouses</option>
-          <option value="upper_west">Upper West Cedar St Townhouses</option>
-          <option value="fulton">Fulton Street Townhouses</option>
-          <option value="talmadge">Talmadge Court</option>
-          <option value="new_fulton">New Fulton Townhouses</option>
-        </select>
-        <br>
-        
-        <input type="checkbox" name="special-needs" value="true">Special Needs<br>
-        <input type="checkbox" name="laundry" value="true">Laundry on Premise<br>
-        <input type="checkbox" name="kitchen" value="true">Fully Equipped Kitchen<br>
-        
-        <input type ="submit" value = "Submit">
+      <input type="checkbox" name="special-needs" value="true">Special Needs<br>
+      <input type="checkbox" name="laundry" value="true">Laundry on Premise<br>
+      <input type="checkbox" name="kitchen" value="true">Fully Equipped Kitchen<br>
+      
+      <input type ="submit" value = "Submit">
       </form>
     </body>
 </HTML>
--- room_selection.php
+++ room_selection.php
@@ -1,20 +1,107 @@
 <html>
-    <head>
-        <title>Room Selection Results</title>
-    </head>
-    <?php
-        $hallSelection = isset($_POST["residence"]);
-        $gender = $_POST["gender"];
-        $class = $_POST["class"];
-        $specialNeeds = isset($_POST["special-needs"]);
-        $laundry = isset($_POST["laundry"]);
-        $kitchen = isset($_POST["kitchen"]);
-        
-        if($hallSelection){
-            echo "true";
-        } else{
-            echo "false";
+  <head>
+    <title>Room Selection Results</title>
+  </head>
+  <?php
+  $hallSelection = isset($_POST["residence"]);
+  $gender = $_POST["gender"];
+  $class = $_POST["class"];
+  $specialNeeds = isset($_POST["special-needs"]);
+  $laundry = isset($_POST["laundry"]);
+  $kitchen = isset($_POST["kitchen"]);
+  
+  if($hallSelection){
+    $verify=false;
+    switch($hallSelection){
+      case "leo":
+        if($class=="freshman"&&(!$specialNeeds)&&(!$kitchen)){
+          $verify=true;
         }
-        
-    ?>
+        break;
+      case "champagnat":
+        if($class=="freshman"&&(!$specialNeeds)&&(!$kitchen)){
+          $verify=true;
+        }
+        break;
+      case "marian":
+        if($class=="freshman"&&(!$specialNeeds)&&(!$kitchen)){
+          $verify=true;
+        }
+        break;
+      case "sheahan":
+        if($class=="freshman"&&(!$specialNeeds)&&(!$kitchen)){
+          $verify=true;
+        }
+        break;
+      case "midrise":
+        if($class=="sophmore"&&(!$specialNeeds)&&(!$kitchen)){
+          $verify=true;
+        }
+        break;
+      case "foy":
+        if($class=="sophmore"&&(!$specialNeeds)){
+          $verify=true;
+        }
+        break;
+      case "gartland":
+        if($class=="sophmore"&&(!$specialNeeds)){
+          $verify=true;
+        }
+        break;
+      case "new":
+        if($class=="sophmore"&&(!$specialNeeds)){
+          $verify=true;
+        }
+        break;
+      case "lower_west":
+        if(($class=="juniors"||$class=="seniors")&&(!$specialNeeds)){
+          $verify=true;
+        }
+        break;
+      case "upper_west":
+        if(($class=="juniors"||$class=="seniors")&&(!$specialNeeds)){
+          $verify=true;
+        }
+        break;
+      case "fulton":
+        if(($class=="juniors"||$class=="seniors")&&(!$specialNeeds)){
+          $verify=true;
+        }
+        break;
+      case "talmadge":
+        if(($class=="juniors"||$class=="seniors")&&(!$specialNeeds)){
+          $verify=true;
+        }
+        break;
+      case "new_fulton":
+        if(($class=="juniors"||$class=="seniors")&&(!$specialNeeds)){
+          $verify=true;
+        }
+        break;
+    }
+    if($verify){
+      echo "You did it!";
+      ?>
+      <form method="get" action="room_confirmation.php">
+        <input type = "hidden" name="hallSelection" value="<?php echo $hallSelection;?>">
+        <input type = "hidden" name="gender" value="<?php echo $gender; ?>">
+        <input type = "hidden" name="class" value="<?php echo $class; ?>">
+        <input type = "hidden" name="special-needs" value="<?php echo $specialNeeds; ?>">
+        <input type = "hidden" name="laundry" value="<? echo $laundry ?>">
+        <input type = "hidden" name="kitchen" value="<?php echo $kitchen; ?>">
+        <input type = "hidden" name="cwid" value="<?php $_POST["cwid"]; ?>">
+        <input type = "hidden" name="lname" value="<?php $_POST["lname"]; ?>">
+        <input type = "hidden" name="fname" value="<?php $_POST["fname"]; ?>">
+        <input type ="submit" value = "Submit">
+      </form>
+      <?php
+    } else{
+      echo "Please redo your selection";
+      echo "<a href=\"index.php\">Select preference again</a>";
+    }
+  } else{
+    echo "false";
+  }
+  
+  ?>
 </html>
