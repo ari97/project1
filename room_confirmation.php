512b4b6c8564402cc70b5bc5ae870b0ae696efb9
mcelevan12 <evan.mcelheny1@marist.edu>
2016-03-03 15:08:57 +0000

    Finish Project

--- room_confirmation.php
+++ room_confirmation.php
@@ -1,28 +1,76 @@
-<html>
-  <head>
-    <title>
-      Room Confirmation
-    </title>
-  </head>
+
   <?php
+  echo "<html>
+  <head>
+    <title>Room Confirmation</title>
+  </head>
+  <body>"; 
   $fname = $_POST["fname"];
   $lname = $_POST["lname"];
   $cwid = $_POST["cwid"];
-  $hallSelection = $_POST["hallSelection"];
+  $residence = $_POST["residence"];
   $gender = $_POST["gender"];
   $class = $_POST["class"];
   $specialNeeds = $_POST["special-needs"]; 
   $laundry = $_POST["laundry"];
   $kitchen = $_POST["kitchen"];
   
+  
+  //Change the $residence from the temp name to actual name:
+  
+  switch ($residence){
+    case "Leo":
+      $residence = "Leo Hall";
+      break;
+    case "Champagnat":
+      $residence = "Champagnat Hall";
+      break;
+    case "Marian":
+      $residence = "Marian Hall";
+      break;
+    case "Sheahan":
+      $residence = "Shehan Hall";
+      break;
+    case "midrise":
+      $residence = "Midrise Hall";
+      break;
+    case "foy":
+      $residence = "Foy Townhouses";
+      break;
+    case "gartland":
+      $residence = "Gartland Commons";
+      break;
+    case "new":
+      $residence = "New Townhouses";
+      break;
+    case "lower_west":
+      $residence = "Lower West Cedar St Townhouses";
+      break;
+    case "upper_west":
+      $residence = "Upper West Cedar St Townhouses";
+      break;
+    case "fulton":
+      $residence = "Fulton St Townhouses";
+      break;
+    case "talmadge":
+      $residence = "Talmadge Court";
+      break;
+    case "new_fulton":
+      $residence = "New Fulton Townhouses";
+      break;
+  }
+  
   echo "First name: ", $fname. "\n<br>\n";
   echo "Last name: ", $lname, "\n<br>\n";
   echo "CWID: ", $cwid, "\n<br>\n";
-  echo "Hall Selection: ", $hallSelection, "\n<br>\n";
+  echo "Hall Selection: ", $residence, "\n<br>\n";
   echo "Gender: ", $gender, "\n<br>\n";
   echo "Class: ", $class, "\n<br>\n";
   echo "Special-needs: ", ($specialNeeds?"True": "False"), "\n<br>\n";
   echo "Laundry: ", ($laundry?"True": "False"), "\n<br>\n";
   echo "Kitchen: ", ($kitchen?"True": "False"), "\n<br>\n";
+  
+  echo"
+  </body>
+</html>";
   ?>
-</html>
--- test.php
+++ test.php
@@ -1,15 +1,69 @@
+
 <?php
+session_start();
+$pagecount = $_SESSIONT["pagecount"];
+echo "<!--".$pagecount."-->";
+if($pagecount==0) {
+  echo "
+<html>
+    <head>
+      <title>Room Selection Form</title>
+    </head>
+  <body>
+    <form action=\"index.php\" method=\"POST\">
+      First name: <input type=\"text\" name=\"fname\"><br>
+      Last name: <input type = \"text\" name = \"lname\"><br>
+      CWID: <input type = \"text\" name = \"cwid\"><br>
+      <input type = \"radio\" name=\"gender\" value = \"male\"> Male<br>
+      <input type = \"radio\" name=\"gender\" value = \"female\"> Female<br>
+      <input type = \"radio\" name=\"gender\" value = \"other\"> Other<br>
+      <br>
+      <input type = \"radio\" name=\"class\" value = \"Freshman\"> Freshman<br>
+      <input type = \"radio\" name=\"class\" value = \"Sophomore\"> Sophomore<br>
+      <input type = \"radio\" name=\"class\" value = \"Junior\"> Junior<br>
+      <input type = \"radio\" name=\"class\" value = \"Senior\"> Senior<br>
+      <br>
+      <select name = \"residence\">
+        <option selected disabled hidden value=''>Select a Residence Hall</option>
+        <option value=\"Leo\">Leo Hall</option>
+        <option value=\"Champagnat\">Champagnat Hall</option>
+        <option value=\"Marian\">Marian Hall</option>
+        <option value=\"Sheahan\">Sheahan Hall</option>
+        <option value=\"midrise\">Midrise Hall</option>
+        <option value=\"foy\">Foy Townhouses</option>
+        <option value=\"gartland\">Gartland Commons</option>
+        <option value=\"new\">New Townhouses</option>
+        <option value=\"lower_west\">Lower West Cedar St Townhouses</option>
+        <option value=\"upper_west\">Upper West Cedar St Townhouses</option>
+        <option value=\"fulton\">Fulton Street Townhouses</option>
+        <option value=\"talmadge\">Talmadge Court</option>
+        <option value=\"new_fulton\">New Fulton Townhouses</option>
+      </select>
+      <br>
+      <input type=\"checkbox\" name=\"special-needs\" value=\"true\">Special Needs<br>
+      <input type=\"checkbox\" name=\"laundry\" value=\"true\">Laundry on Premise<br>
+      <input type=\"checkbox\" name=\"kitchen\" value=\"true\">Fully Equipped Kitchen<br>
+      <input type = \"hidden\" name=\"pagecount\" value=\"" . 1 . "\">
+      <input type =\"submit\" value = \"Submit\">
+    </form>
+  </body>
+</html>";
+} elseif($pagecount==1){  
   echo "<html>
   <head>
     <title>Room Selection Results</title>
   </head>
   <body>"; 
-  $residence = $_POST["residence"];
-  $gender = $_POST["gender"];
-  $class = $_POST["class"];
-  $specialNeeds = $_POST["special-needs"];
-  $laundry = $_POST["laundry"];
-  $kitchen = $_POST["kitchen"];
+  $_SESSION["fname"] = $_POST["fname"];
+  $_SESSION["lname"] = $_POST["lname"];
+  $_SESSION["cwid"] = $_POST["cwid"];
+  $_SESSION["gender"] = $_POST["gender"];
+  $_SESSION["class"] = $_POST["class"];
+  $_SESSION["residence"] = $_POST["residence"];
+  $_SESSION["gender"] = $_POST["gender"];
+  $_SESSION["specialNeeds"] = $_POST["special-needs"];
+  $_SESSION["laundry"] = $_POST["laundry"];
+  $_SESSION["kitchen"] = $_POST["kitchen"];
   
   if($residence){
     $verify=false;
@@ -81,19 +135,8 @@
         break;
     }
     if($verify){
-      echo "Your room selection is available with your given information.
-      <form action=\"room_confirmation.php\" method=\"POST\">
-        <input type = \"hidden\" name=\"residence\" value=\"" . $residence . "\">
-        <input type = \"hidden\" name=\"gender\" value=\"" . $gender . "\">
-        <input type = \"hidden\" name=\"class\" value=\"" . $class . "\">
-        <input type = \"hidden\" name=\"special-needs\" value=\"" . $specialNeeds . "\">
-        <input type = \"hidden\" name=\"laundry\" value=\"" . $laundry . "\">
-        <input type = \"hidden\" name=\"kitchen\" value=\"" . $kitchen . "\">
-        <input type = \"hidden\" name=\"cwid\" value=\"" . $_POST["cwid"] . "\">
-        <input type = \"hidden\" name=\"lname\" value=\"" . $_POST["lname"] . "\">
-        <input type = \"hidden\" name=\"fname\" value=\"" . $_POST["fname"] . "\">
-        <input type =\"submit\" value = \"Submit\">
-      </form>";
+      $_SESSION["pagecount"]=2;
+      header('Location: test.php');
     } else{
       echo "Please redo your selection";
       $tclass = true;
@@ -156,7 +199,6 @@
           break;
       }
       echo "<br>";
-      //Change the $residence from the temp name to the actual full name:
        switch ($residence){
         case "Leo":
           $residence = "Leo Hall";
@@ -225,7 +267,7 @@
               "Fulton Street", "talmadge"=>"Talmadge Court", "new_fulton"=>"New Fulton Townhouses");
           break;
       }
-      echo "   <form action=\"room_confirmation.php\" method=\"POST\">
+      echo "   <form action=\"index.php\" method=\"POST\">
       <select name = \"residence\">";
       foreach($choices as $k=>$v){
         echo "        <option value=\"".$k."\">".$v."</option>";
@@ -239,12 +281,13 @@
       <input type = \"hidden\" name=\"cwid\" value=\"" . $_POST["cwid"] . "\">
       <input type = \"hidden\" name=\"lname\" value=\"" . $_POST["lname"] . "\">
       <input type = \"hidden\" name=\"fname\" value=\"" . $_POST["fname"] . "\">
+      <input type = \"hidden\" name=\"pagecount\" value=\"" . 2 . "\">
       <input type =\"submit\" value = \"Submit\">
     </form>";
     }else{
       echo "Please select a class:
     <br>
-    <form action=\"room_selection.php\" method=\"POST\">
+    <form action=\"index.php\" method=\"POST\">
       <input type = \"radio\" name=\"class\" value = \"Freshman\"> Freshman<br>
       <input type = \"radio\" name=\"class\" value = \"Sophomore\"> Sophomore<br>
       <input type = \"radio\" name=\"class\" value = \"Junior\"> Junior<br>
@@ -257,6 +300,7 @@
       <input type = \"hidden\" name=\"cwid\" value=\"" . $_POST["cwid"] . "\">
       <input type = \"hidden\" name=\"lname\" value=\"" . $_POST["lname"] . "\">
       <input type = \"hidden\" name=\"fname\" value=\"" . $_POST["fname"] . "\">
+      <input type = \"hidden\" name=\"pagecount\" value=\"" . 1 . "\">
       <input type = \"submit\" value = \"Submit\">
     </form>";
     }
@@ -264,4 +308,84 @@
   echo "
   </body>
 </html>";
-  ?>
+} else if($pagecount==2){
+  echo "<html>
+  <head>
+    <title>Room Confirmation</title>
+  </head>
+  <body>"; 
+  $fname = $_POST["fname"];
+  $lname = $_POST["lname"];
+  $cwid = $_POST["cwid"];
+  $residence = $_POST["residence"];
+  $gender = $_POST["gender"];
+  $class = $_POST["class"];
+  $specialNeeds = $_POST["special-needs"]; 
+  $laundry = $_POST["laundry"];
+  $kitchen = $_POST["kitchen"];
+  
+  switch ($residence){
+    case "Leo":
+      $residence = "Leo Hall";
+      break;
+    case "Champagnat":
+      $residence = "Champagnat Hall";
+      break;
+    case "Marian":
+      $residence = "Marian Hall";
+      break;
+    case "Sheahan":
+      $residence = "Shehan Hall";
+      break;
+    case "midrise":
+      $residence = "Midrise Hall";
+      break;
+    case "foy":
+      $residence = "Foy Townhouses";
+      break;
+    case "gartland":
+      $residence = "Gartland Commons";
+      break;
+    case "new":
+      $residence = "New Townhouses";
+      break;
+    case "lower_west":
+      $residence = "Lower West Cedar St Townhouses";
+      break;
+    case "upper_west":
+      $residence = "Upper West Cedar St Townhouses";
+      break;
+    case "fulton":
+      $residence = "Fulton St Townhouses";
+      break;
+    case "talmadge":
+      $residence = "Talmadge Court";
+      break;
+    case "new_fulton":
+      $residence = "New Fulton Townhouses";
+      break;
+  }
+  
+  echo "    First name: ", $fname. "
+    <br>
+    Last name: ", $lname, "
+    <br>
+    CWID: ", $cwid, "
+    <br>
+    Hall Selection: ", $residence, "
+    <br>
+    Gender: ", $gender, "
+    <br>
+    Class: ", $class, "
+    <br>
+    Special-needs: ", ($specialNeeds?"True": "False"), "
+    <br>
+    Laundry: ", ($laundry?"True": "False"), "
+    <br>
+    Kitchen: ", ($kitchen?"True": "False"), "
+    <br>
+  </body>
+</html>";
+}
+?>
+
